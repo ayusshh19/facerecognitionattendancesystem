56f7c30ec12f62920ea11504cbb70237373448cf
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2022-11-14 16:42:10 +0530

    sidebar created

---
+++ facerecogattend/attendance/static/js/sidebar.js
@@ -0,0 +1,4 @@
+document.getElementById('sidebar-bottom').addEventListener('click',()=>{
+    console.log('clicked')
+    document.getElementById('sidebar-container').classList.toggle('active')
+})
--- facerecogattend/facerecogattend/settings.py
+++ facerecogattend/facerecogattend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'attendance'
 ]
 
 MIDDLEWARE = [
@@ -50,11 +52,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'facerecogattend.urls'
-
+TEMPLATES_DIR=os.path.join(BASE_DIR,'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,9 +116,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_DIR=os.path.join(BASE_DIR,'static')
 STATIC_URL = 'static/'
-
+STATICFILES_DIR=[STATIC_DIR]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
